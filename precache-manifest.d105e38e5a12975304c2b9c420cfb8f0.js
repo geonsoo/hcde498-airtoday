31b7234cc5b7ca1b7ce439b21e3e2e009fc0b11f
Geon Soo Park <geonsoo@uw.edu>
2019-02-22 13:28:04 -0800

    Updates

---
+++ precache-manifest.d105e38e5a12975304c2b9c420cfb8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/geonsoo/hcde498-airtoday.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3d692eded8fb43f7912",
+    "url": "/geonsoo/hcde498-airtoday.git/static/js/runtime~main.c3d692ed.js"
+  },
+  {
+    "revision": "2b029f01aa4cfff12a10",
+    "url": "/geonsoo/hcde498-airtoday.git/static/js/main.2b029f01.chunk.js"
+  },
+  {
+    "revision": "48fed45ea8ce500caa6f",
+    "url": "/geonsoo/hcde498-airtoday.git/static/js/2.48fed45e.chunk.js"
+  },
+  {
+    "revision": "2b029f01aa4cfff12a10",
+    "url": "/geonsoo/hcde498-airtoday.git/static/css/main.ae8f1ed6.chunk.css"
+  },
+  {
+    "revision": "afb9c371bf14143c0871a280eebd1a69",
+    "url": "/geonsoo/hcde498-airtoday.git/index.html"
+  }
+];
