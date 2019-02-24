055f3db480842c1850e28d25674fb07f4f8f2919
Geon Soo Park <geonsoo@uw.edu>
2019-02-23 17:14:30 -0800

    project saturday

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
 // import Navbar from 'react-bootstrap/Navbar';
 // import Nav from 'react-bootstrap/Nav';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Col, Container, Row } from 'react-bootstrap';
 
 
 class App extends Component {
@@ -19,27 +19,68 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
-          <Navbar bg="dark" variant="dark">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark" fixed="top">
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#features">Features</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
-      
+         
     
           </Navbar>
-        
-          <p>
-            number of times pressed: {this.state.numClicks}
-          </p>
-       
-            Hello There
-          
-        <header className="App-header">
+          </header>
+         
+          <body>
+            <div className = "wrapper scene_one"  >        
+              <Container className = "first main_page">
+                <Row>
+                  <Col class="align-middle">The air around you is changing. Wii you help stop it?</Col>
+               </Row>
+               
+    
+              </Container>
+            </div>
+            <div className = "wrapper scene_two"  >  
+              <Container className = "second">
+                <Row>
+                  <Col>1 of 2</Col>
+                  <Col>2 of 2</Col>
+                </Row>
+    
+              </Container>
+            </div>
+            <div className = "wrapper scene_three"  >  
+              <Container className = "third">
+                <Row>
+                  <Col>1 of 2</Col>
+                  <Col>2 of 2</Col>
+                  </Row>
+    
+              </Container>
+            </div>
+            <div className = "wrapper scene_four"  >  
 
-        </header>
+              <Container className = "fourth">
+                <Row>
+                  <Col>1 of 2</Col>
+                  <Col>2 of 2</Col>
+                  </Row>
+    
+              </Container>
+            </div>
+            <div className = "wrapper scene_five"  >  
+              <Container className = "fifth">
+                <Row>
+                  <Col>1 of 2</Col>
+                  <Col>2 of 2</Col>
+                  </Row>
+    
+              </Container>
+            </div>
+          
+         </body>
       </div>
     );
   }
